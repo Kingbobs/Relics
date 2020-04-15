eb41de2239b5f9e7f9287595f4fc17d0b696c4ca
KingBob <52259912+Kingbobs@users.noreply.github.com>
2020-04-14 23:41:12 -0600

    Update Main.php

--- src/Kingbobs/relics/Main.php
+++ src/Kingbobs/relics/Main.php
@@ -35,27 +35,27 @@ class Main extends PluginBase implements Listener{
 		    
 			$chance = mt_rand(0, 500);
 			/**
-			 * Common 75%
-			 * epic 60%
-			 * rare 40%
-			 * legendary 20%
-			 * mythical 10%
+			 * Common 50%
+			 * epic 25%
+			 * rare 15%
+			 * legendary 10%
+			 * mythical 5%
 			 */
-			if ($chance > 75 && $chance <= 76){
+			if ($chance > 50 && $chance <= 76){
 				$event->getPlayer()->sendMessage($this->config["Common"]["Message"]["Receive"]);
 				$this->giveRelic($event->getPlayer(), "common");
 			}
-			if ($chance > 60 && $chance <= 60){
+			if ($chance > 25 && $chance <= 60){
 				$event->getPlayer()->sendMessage($this->config["Epic"]["Message"]["Receive"]);
 				$this->giveRelic($event->getPlayer(), "epic");
 			}
-			if ($chance > 50 && $chance <= 40){
+			if ($chance > 15 && $chance <= 40){
 				$this->giveRelic($event->getPlayer(), "rare");
 			}
-			if ($chance > 30 && $chance <= 20){
+			if ($chance > 10 && $chance <= 30){
 				$this->giveRelic($event->getPlayer(), "legendary");
 			}
-			if ($chance > 20 && $chance <= 10){
+			if ($chance > 5 && $chance <= 20){
 				$this->giveRelic($event->getPlayer(), "mythical");
 			}
 		}
